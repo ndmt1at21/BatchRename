8e2b225c02932753a240a3e95e875417d607e416
ndmt1at21 <trminh36@gmail.com>
2021-11-06 18:11:31 +0700

    add window style

---
+++ BatchRename/CustomControl/CustomButton.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace BatchRename.CustomControl
+{
+    public class CustomButton : Button
+    {
+        public static readonly DependencyProperty CornerRadiusProperty =
+           DependencyProperty.Register("CornerRadius", typeof(CornerRadius), typeof(Button));
+
+        public static readonly DependencyProperty HoverBackgroundProperty =
+           DependencyProperty.Register("HoverBackground", typeof(SolidColorBrush), typeof(Button));
+
+        public static readonly DependencyProperty ActiveBackgroundProperty =
+           DependencyProperty.Register("ActiveBackground", typeof(SolidColorBrush), typeof(Button));
+
+        public static readonly DependencyProperty DisableBackgroundProperty =
+          DependencyProperty.Register("DisableBackground", typeof(SolidColorBrush), typeof(Button));
+
+        public CornerRadius CornerRadius
+        {
+            get => (CornerRadius)GetValue(CornerRadiusProperty);
+            set => SetValue(CornerRadiusProperty, value);
+        }
+
+        public SolidColorBrush HoverBackground
+        {
+            get => (SolidColorBrush)GetValue(HoverBackgroundProperty);
+            set => SetValue(HoverBackgroundProperty, value);
+        }
+
+        public SolidColorBrush ActiveBackground
+        {
+            get => (SolidColorBrush)GetValue(ActiveBackgroundProperty);
+            set => SetValue(ActiveBackgroundProperty, value);
+        }
+
+        public SolidColorBrush DisableBackground
+        {
+            get => (SolidColorBrush)GetValue(DisableBackgroundProperty);
+            set => SetValue(DisableBackgroundProperty, value);
+        }
+
+        static CustomButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomButton), new FrameworkPropertyMetadata(typeof(CustomButton)));
+        }
+    }
+}
