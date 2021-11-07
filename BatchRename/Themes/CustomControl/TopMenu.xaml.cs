5b6af299dc1a7331c1f83920c8a49dca2bd4cd3c
ndmt1at21 <trminh36@gmail.com>
2021-11-07 10:44:50 +0700

    refactor custom window

---
+++ BatchRename/Themes/CustomControl/TopMenu.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Controls;
+
+namespace BatchRename.Themes.CustomControl
+{
+    public delegate void HandlerConvertClick();
+
+    public partial class TopMenu : UserControl
+    {
+        public HandlerConvertClick OnConvertClick;
+
+        public TopMenu()
+        {
+            InitializeComponent();
+        }
+
+        public TopMenu(HandlerConvertClick onConvertClick)
+        {
+            OnConvertClick += onConvertClick;
+        }
+    }
+}
---
+++ BatchRename/Themes/CustomControl/WindowCommandButtons.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BatchRename.Themes.CustomControl
+{
+    /// <summary>
+    /// Interaction logic for WindowCommandButtons.xaml
+    /// </summary>
+    public partial class WindowCommandButtons : UserControl
+    {
+        public static readonly DependencyProperty ShowMinimizeProperty =
+            DependencyProperty.Register(
+                "ShowMinimizeBackground",
+                typeof(Boolean),
+                typeof(StackPanel),
+                new PropertyMetadata(true)
+            );
+
+        public static readonly DependencyProperty ShowRestoreProperty =
+          DependencyProperty.Register(
+              "ShowRestoreBackground",
+              typeof(Boolean),
+              typeof(StackPanel),
+              new PropertyMetadata(true)
+          );
+
+        public Boolean ShowMinimize
+        {
+            get => (Boolean)GetValue(ShowMinimizeProperty);
+            set => SetValue(ShowMinimizeProperty, value);
+        }
+
+        public Boolean ShowRestore
+        {
+            get => (Boolean)GetValue(ShowRestoreProperty);
+            set => SetValue(ShowRestoreProperty, value);
+        }
+
+        public WindowCommandButtons()
+        {
+            InitializeComponent();
+        }
+
+        private void MaxBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Window window = Window.GetWindow(this);
+
+            if (window.WindowState == WindowState.Maximized)
+                SystemCommands.RestoreWindow(window);
+
+            if (window.WindowState == WindowState.Normal)
+                SystemCommands.MaximizeWindow(window);
+        }
+
+        private void MinBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Window window = Window.GetWindow(this);
+            SystemCommands.MinimizeWindow(window);
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Window window = Window.GetWindow(this);
+            SystemCommands.CloseWindow(window);
+        }
+    }
+}
