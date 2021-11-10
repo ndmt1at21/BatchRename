015e6cfad1f0744c2f8769c580610da4d136f70d
thanhuy008 <thanhuy008@gmail.com>
2021-11-10 21:29:27 +0700

    MainWindow FilesPanel ver1.0

---
+++ BatchRename/Themes/CustomControl/DropFilePanel.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BatchRename.Themes.CustomControl
+{
+    /// <summary>
+    /// Interaction logic for DropFilePanel.xaml
+    /// </summary>
+    public partial class DropFilePanel : UserControl
+    {
+        public DropFilePanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
