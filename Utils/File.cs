8481c7e34cee78c64718457a0a3a1a99916ccaa7
ndmt1at21 <trminh36@gmail.com>
2021-11-10 21:54:12 +0700

    move utils to new project

---
+++ Utils/File.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Utils
+{
+    public static class File
+    {
+        public static void RenameFile(string currentPath, string newFileName)
+        {
+            FileInfo file = new FileInfo(currentPath);
+
+            if (file.Exists)
+            {
+                try
+                {
+                    var groups = Regex.Match(currentPath, @"^(.+)[\/\\]([^\/]+)$").Groups;
+
+                    if (groups.Count != 3)
+                        throw new NotSupportedException();
+
+                    file.MoveTo(@$"{groups[1]}\{newFileName}");
+                }
+                catch
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
