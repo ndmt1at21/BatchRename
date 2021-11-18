b2a12f9b2b6a740f3f8416ab437169a98f2fb546
ndmt1at21 <trminh36@gmail.com>
2021-11-18 21:38:59 +0700

    remove/add some method

--- ChangeExtensionRule/ChangeExtensionPlugin.cs
+++ ChangeExtensionRule/ChangeExtensionPlugin.cs
@@ -13,24 +13,14 @@ namespace ChangeExtensionRule
 
         public string Name { get; } = "Change Extension";
 
-        public IRuleComponent CreateComponentInstance()
-        {
-            throw new NotImplementedException();
-        }
-
-        public IRuleComponent CreateComponentInstance(string serializeRuleParameter)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IRuleComponent CreateComponentInstance(IRuleParameter ruleParamter)
-        {
-            throw new NotImplementedException();
-        }
-
         public IRenameRule CreateRuleInstance()
         {
             return new ChangeExtensionRule();
+        }
+
+        public IRuleComponent CreateComponentInstance()
+        {
+            return new ChangeExtensionComponent();
         }
     }
 }
