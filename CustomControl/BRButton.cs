a531065dae2603588b79a4707a243873283b7418
ndmt1at21 <trminh36@gmail.com>
2021-12-02 16:29:55 +0700

    feat: lib CustomControl can import from all plugins

---
+++ CustomControl/BRButton.cs
@@ -0,0 +1,50 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace CustomControl
+{
+    public class BRButton : Button
+    {
+        public static readonly DependencyProperty CornerRadiusProperty =
+           DependencyProperty.Register("CornerRadius", typeof(CornerRadius), typeof(System.Windows.Controls.Button));
+
+        public static readonly DependencyProperty HoverBackgroundProperty =
+           DependencyProperty.Register("HoverBackground", typeof(SolidColorBrush), typeof(System.Windows.Controls.Button));
+
+        public static readonly DependencyProperty ActiveBackgroundProperty =
+           DependencyProperty.Register("ActiveBackground", typeof(SolidColorBrush), typeof(System.Windows.Controls.Button));
+
+        public static readonly DependencyProperty DisableBackgroundProperty =
+          DependencyProperty.Register("DisableBackground", typeof(SolidColorBrush), typeof(System.Windows.Controls.Button));
+
+        public CornerRadius CornerRadius
+        {
+            get => (CornerRadius)GetValue(CornerRadiusProperty);
+            set => SetValue(CornerRadiusProperty, value);
+        }
+
+        public SolidColorBrush HoverBackground
+        {
+            get => (SolidColorBrush)GetValue(HoverBackgroundProperty);
+            set => SetValue(HoverBackgroundProperty, value);
+        }
+
+        public SolidColorBrush ActiveBackground
+        {
+            get => (SolidColorBrush)GetValue(ActiveBackgroundProperty);
+            set => SetValue(ActiveBackgroundProperty, value);
+        }
+
+        public SolidColorBrush DisableBackground
+        {
+            get => (SolidColorBrush)GetValue(DisableBackgroundProperty);
+            set => SetValue(DisableBackgroundProperty, value);
+        }
+
+        static BRButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(BRButton), new FrameworkPropertyMetadata(typeof(BRButton)));
+        }
+    }
+}
