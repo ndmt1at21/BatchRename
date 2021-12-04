4231ec6fe8f92a253feb20913812c8ba523e5624
18120580 <quangtho991@gmail.com>
2021-12-05 00:00:06 +0700

    Tho update rule

--- ToPascalCase/ToPascalCaseRule.cs
+++ ToPascalCase/ToPascalCaseRule.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace ToPascalCase
 {
@@ -14,7 +15,7 @@ namespace ToPascalCase
 
         public FileInfor Convert(FileInfor file, IRuleParameter ruleParameter)
         {
-            string newFileName = Utils.Rule.ToPascalCase(Utils.Rule.RemoveUnicode(file.FileName));
+            string newFileName = ToPascalCase(RemoveUnicode(file.FileName));
 
             return new FileInfor
             {
@@ -32,6 +33,46 @@ namespace ToPascalCase
         public string GetStatement(FileInfor file, IRuleParameter ruleParameter)
         {
             return $"To PascalCase and remove unicode from {file.FileName}";
+        }
+        public string ToPascalCase(string original)
+        {
+            Regex invalidCharsRgx = new Regex("[^_a-zA-Z0-9]");
+            Regex whiteSpace = new Regex(@"(?<=\s)");
+            Regex startsWithLowerCaseChar = new Regex("^[a-z]");
+            Regex firstCharFollowedByUpperCasesOnly = new Regex("(?<=[A-Z])[A-Z0-9]+$");
+            Regex lowerCaseNextToNumber = new Regex("(?<=[0-9])[a-z]");
+            Regex upperCaseInside = new Regex("(?<=[A-Z])[A-Z]+?((?=[A-Z][a-z])|(?=[0-9]))");
+            var pascalCase = invalidCharsRgx.Replace(whiteSpace.Replace(original, "_"), string.Empty)
+                .Split(new char[] { '_' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(w => startsWithLowerCaseChar.Replace(w, m => m.Value.ToUpper()))
+                .Select(w => firstCharFollowedByUpperCasesOnly.Replace(w, m => m.Value.ToLower()))
+                .Select(w => lowerCaseNextToNumber.Replace(w, m => m.Value.ToUpper()))
+                .Select(w => upperCaseInside.Replace(w, m => m.Value.ToLower()));
+
+            return string.Concat(pascalCase);
+        }
+        public string RemoveUnicode(string text)
+        {
+            string[] arr1 = new string[] { "á", "à", "ả", "ã", "ạ", "â", "ấ", "ầ", "ẩ", "ẫ", "ậ", "ă", "ắ", "ằ", "ẳ", "ẵ", "ặ",
+                                           "đ",
+                                           "é","è","ẻ","ẽ","ẹ","ê","ế","ề","ể","ễ","ệ",
+                                           "í","ì","ỉ","ĩ","ị",
+                                           "ó","ò","ỏ","õ","ọ","ô","ố","ồ","ổ","ỗ","ộ","ơ","ớ","ờ","ở","ỡ","ợ",
+                                           "ú","ù","ủ","ũ","ụ","ư","ứ","ừ","ử","ữ","ự",
+                                           "ý","ỳ","ỷ","ỹ","ỵ",};
+            string[] arr2 = new string[] { "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a",
+                                           "d",
+                                           "e","e","e","e","e","e","e","e","e","e","e",
+                                           "i","i","i","i","i",
+                                           "o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o","o",
+                                           "u","u","u","u","u","u","u","u","u","u","u",
+                                           "y","y","y","y","y",};
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                text = text.Replace(arr1[i], arr2[i]);
+                text = text.Replace(arr1[i].ToUpper(), arr2[i].ToUpper());
+            }
+            return text;
         }
     }
 }
