96dc394dcb0db60fc5311d1ddf2a0c776e4c339a
ndmt1at21 <trminh36@gmail.com>
2021-12-06 14:54:40 +0700

    add logic to RulwWindow

--- BatchRename/View/MainWindow.xaml.cs
+++ BatchRename/View/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using BatchRename.Lib;
 using BatchRename.Themes.CustomControl;
+using BatchRename.View;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -36,6 +38,31 @@ namespace BatchRename
                 MessageBox.Show(p);
                 return p;
             });
+        }
+
+        private void btnRuleWindow_Click(object sender, RoutedEventArgs e)
+        {
+            string[] pluginIds = PluginManager.Shared.GetPluginIDs();
+
+            List<RuleComponent> ruleComponents = new List<RuleComponent>();
+
+            foreach (string id in pluginIds)
+            {
+                ruleComponents.Add(
+                    new RuleComponent
+                    {
+                        Id = id,
+                        Name = PluginManager.Shared.GetRuleName(id),
+                        Component = PluginManager.Shared.CreateRuleComponent(id)
+                    });
+                Debug.WriteLine(id);
+            }
+
+
+
+            RuleWindow ruleWindow = new RuleWindow(ruleComponents);
+
+            ruleWindow.Show();
         }
     }
 }
--- BatchRename/View/RuleWindow.xaml.cs
+++ BatchRename/View/RuleWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,22 +12,50 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using PluginContract;
 
 namespace BatchRename.View
 {
-    /// <summary>
-    /// Interaction logic for RuleWindow.xaml
-    /// </summary>
+    public class RuleComponent
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public IRuleComponent Component { get; set; }
+    }
+
     public partial class RuleWindow : Window
     {
-        public RuleWindow()
+        public List<RuleComponent> RuleComponents;
+
+        private BindingList<TabItem> tabItems = new BindingList<TabItem>();
+
+        public RuleWindow(List<RuleComponent> ruleComponents)
         {
             InitializeComponent();
+
+            if (ruleComponents == null)
+                return;
+
+            RuleComponents = ruleComponents;
+
+            foreach (var ruleComponent in RuleComponents)
+            {
+                tabItems.Add(new TabItem
+                {
+                    Header = ruleComponent.Name,
+                    Content = ruleComponent.Component
+                });
+            }
         }
 
         private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            tabControlRule.ItemsSource = tabItems;
         }
     }
 }
