7e518393362529549a1345364c72b0503c3d3bb9
ndmt1at21 <trminh36@gmail.com>
2021-12-07 16:32:44 +0700

    add breakpoint converter

---
+++ BatchRename/Lib/BreakpointConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace BatchRename.Lib
+{
+    public class BreakpointConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            float fValue;
+
+            if (float.TryParse(value.ToString(), out fValue))
+            {
+                Debug.WriteLine(fValue);
+                Debug.WriteLine(parameter);
+
+                if (fValue >= 1920)
+                    return "ExtraLarge";
+
+                if (fValue >= 1200)
+                    return "Large";
+
+                if (fValue >= 992)
+                    return "Normal";
+
+                if (fValue >= 768)
+                    return "Medium";
+
+                if (fValue >= 600)
+                    return "Small";
+            }
+
+            return "Small";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Debug.WriteLine("In Convert Back");
+            return value;
+        }
+    }
+}
